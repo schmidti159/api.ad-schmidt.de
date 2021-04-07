532acdd8a7d389c51109d30a871b40812fae1582
Daniel Schmidt <daniel-git@ad-schmidt.de>
2021-04-07 14:00:26 +0200

    clean repo and fix checkout of different branches

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from secrets import API_SECRET
 
 app = Flask(__name__)
 
-VALID_BRANCH_NAMES=['pre-prod','master']
+VALID_BRANCH_NAMES=['pre-prod','main']
 VALID_REPO_NAMES=['schmidti159/blog.ad-schmidt.de']
 
 @app.route("/")
@@ -26,20 +26,35 @@ def githubWebhook():
 
     content = request.json
     branch = content['ref'].partition('refs/heads/')[2]
-    if not branch in VALID_BRANCH_NAMES:
-        print(branch+' does not match any of the branches in '+str(VALID_BRANCH_NAMES)+'. Skipping this event.')
+    if not checkBranchAndRepo(content, branch):
         return 'skipped'
-    if not content['repository']['full_name'] in VALID_REPO_NAMES:
-        print(content['repository']['full_name']+' does not match any of the repos in '+str(VALID_REPO_NAMES)+'. Skipping this event.')
-        return 'skipped'
-    # check out and compile content
-    subprocess.run(['git','pull','origin',branch], stdout=sys.stdout, stdin=sys.stdin, cwd='blog.ad-schmidt.de')
-    subprocess.run(['bash','deploy.sh'], stdout=sys.stdout, stdin=sys.stdin, cwd='blog.ad-schmidt.de')
+    checkoutAndDeploy(branch)
     return "ok"
 
 def verifySignature():
     signature = "sha256="+hmac.new(bytes(API_SECRET , 'utf-8'), msg = request.data, digestmod = hashlib.sha256).hexdigest().lower()
     return hmac.compare_digest(signature, request.headers['X-Hub-Signature-256'])
+
+def checkBranchAndRepo(content, branch):
+    if not branch in VALID_BRANCH_NAMES:
+        print(branch+' does not match any of the branches in '+str(VALID_BRANCH_NAMES)+'. Skipping this event.')
+        return False
+    if not content['repository']['full_name'] in VALID_REPO_NAMES:
+        print(content['repository']['full_name']+' does not match any of the repos in '+str(VALID_REPO_NAMES)+'. Skipping this event.')
+        return False 
+    return True
+
+
+def checkoutAndDeploy(branch):
+    # clean the repo
+    subprocess.run(['git','reset','--hard'], stdout=sys.stdout, stdin=sys.stdin, cwd='blog.ad-schmidt.de')
+    subprocess.run(['git','clean','-fd'], stdout=sys.stdout, stdin=sys.stdin, cwd='blog.ad-schmidt.de')
+    # checkout
+    subprocess.run(['git','checkout','-b',branch], stdout=sys.stdout, stdin=sys.stdin, cwd='blog.ad-schmidt.de')
+    subprocess.run(['git','pull','origin',branch], stdout=sys.stdout, stdin=sys.stdin, cwd='blog.ad-schmidt.de')
+    # deploy
+    subprocess.run(['bash','deploy.sh'], stdout=sys.stdout, stdin=sys.stdin, cwd='blog.ad-schmidt.de')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9003, debug=True)
